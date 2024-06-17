dcafb20abb1e3cacc575af372b0b862ab205277e
dy222175 <108139948+dy222175@users.noreply.github.com>
2024-06-18 01:50:41 +0530

    viteconfig changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // plugins: [react()],
   plugins: [react()],
+server: {
+host: true,
+strictPort: true,
+port: 8000,
+},
+  resolve: {}
 })
